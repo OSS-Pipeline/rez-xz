6fd883b8ab0183b1936ac5fa9bb876a56ad4a409
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-10-08 14:27:15 -0400

    Adding support for PKG_CONFIG_PATH

--- package.py
+++ package.py
@@ -57,6 +57,7 @@ uuid = "xz-{version}".format(version=str(version))
 def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib/pkgconfig")
 
     # Helper environment variables.
     env.XZ_BINARY_PATH.set("{root}/bin")
